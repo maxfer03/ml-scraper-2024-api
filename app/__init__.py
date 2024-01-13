f91eb5eb4806817a9dc8c128613f123afd2c84bc
Max <sandsferdinand@gmail.com>
2024-01-13 17:07:36 -0300

    added cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 # Initialize the Flask application
 app = Flask(__name__)
-
+CORS(app)
 # Importing the routes from the 'app' module
 from app import routes
