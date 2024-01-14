78b91cdea3c8217f3cd78ee6bca5a900ebdf7c54
Max <sandsferdinand@gmail.com>
2024-01-14 13:49:22 -0300

    basic serverless architecture

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import awsgi
 from flask import Flask
 from flask_cors import CORS
 
@@ -6,3 +7,6 @@ app = Flask(__name__)
 CORS(app)
 # Importing the routes from the 'app' module
 from app import routes
+
+def lambda_handler(event, context):
+    return awsgi.response(app, event, context, base64_content_types={"image/png"})
